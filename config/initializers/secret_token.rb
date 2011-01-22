3061aeb0f8fbb24e1918d252320e849be7db6bbb
Jacob Foshee <jacobf@gmail.com>
2011-01-22 14:34:41 -0600

    Initial commit for nuvolos project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nuvolos::Application.config.secret_token = '34b4bc2a44fb2bd0903a49bdfa6f753308fac80d316a7630f4eb4558ccf861bdb47f074b50a5b42aca2f97d1d43b48dd80a41035447f1f1571781842444e027e'
